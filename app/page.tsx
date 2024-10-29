521d399643eb8ac12aa2d26c93bc1bf45ec78d8a
Your Name <you@example.com>
2024-10-29 03:34:24 -0700

    update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,9 @@ const Page = async () => {
         <>
           <div>
             <Heading
-              title="Becodemy - Admin"
-              description="Becodemy is a platform for students to learn and get help from teachers"
-              keywords="Programming,MERN,Redux,Machine Learning"
+              title="PROMPT HUB - Admin"
+              description="PROMPTHUB is a place to buy and sell AI images prompts"
+              keywords="ai,images,prompt., Prompt"
             />
             <div className="flex min-h-screen">
               <div className="2xl:w-[16%] w-1/5">
